02e7049fe582b1336b221f18b017280b2a915c05
Luan Albuquerque <luan.santos6605@gmail.com>
2024-04-27 22:57:21 -0400

    feat/setup - swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './app/config/swagger.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  setupSwagger(app);
+  
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(
+      `Server is running on port http://${process.env.HOST}:${process.env.PORT}`,
+    );
+    console.log(
+      `Swagger running on port http://${process.env.HOST}:${process.env.PORT}/api/docs`,
+    );
+  });
 }
 bootstrap();
+
+
